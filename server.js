c886d1cb7d64233bc8569401bd14e30f110efbb6
rudxra <rudxradev@gmail.com>
2024-05-08 22:42:46 +0530

    first

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
-require('dotenv').config(); // Load environment variables
+const emailUser = process.env.EMAIL_USER;
+const emailPass = process.env.EMAIL_PASS;
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail', // If using Gmail
   auth: {
-    user: "rssnow284@gmail.com", // Your Gmail address
-    pass: "fssdxchjwrcnyrpz", // Your app-specific password
+    user:emailUser, 
+    pass:emailPass,
   },
 });
 
@@ -28,18 +31,19 @@ contactEmail.verify((error) => {
 });
 
 router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
-  const email = req.body.email;
-  const message = req.body.message;
-  const phone = req.body.phone;
+  const { firstName, lastName, email, message, phone } = req.body;
+
+  // Construct email details
   const mail = {
-    from: name,
+    from: `${firstName} ${lastName}`,
     to: "rssnow284@gmail.com",
     subject: "Contact Form Submission - Portfolio",
-    html: `<p>Name: ${name}</p>
-           <p>Email: ${email}</p>
-           <p>Phone: ${phone}</p>
-           <p>Message: ${message}</p>`,
+    html: `
+      <p>Name: ${firstName} ${lastName}</p>
+      <p>Email: ${email}</p>
+      <p>Phone: ${phone}</p>
+      <p>Message: ${message}</p>
+    `,
   };
   contactEmail.sendMail(mail, (error) => {
     if (error) {
